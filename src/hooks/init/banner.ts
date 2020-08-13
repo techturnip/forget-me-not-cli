bfcebcc4f6c01336d2427fc7d0593b135db523f5
Tyler Turnipseed <tyler@techturnip.us>
2020-08-13 12:54:59 -0400

    install testing libraries, refactor banner hook

--- src/hooks/init/banner.ts
+++ src/hooks/init/banner.ts
@@ -5,17 +5,26 @@ const gradient = require('gradient-string')
 const figlet = require('figlet')
 
 const hook: Hook<'init'> = async function (opts) {
-  if (typeof(opts.id) !== 'string') {
+  // if opts.id is a string we assign the value to id
+  // so that we can check the length if a value exists
+  const id = opts.id ? opts.id : ''
+
+  // if id does not have a length then there is no command
+  // meaning we are simply using the base command
+  if (id.length === 0) {
+    // we print out the figlet banner with a gradient applied
     process.stdout.write(gradient.passion(figlet.textSync(`FMN CLI v${opts.config.version}`, {
-      font: 'Big',
+      font: 'Big', // sets the figlet font
       horizontalLayout: 'default',
       verticalLayout: 'default',
       width: 80,
       whitespaceBreak: true,
     })))
-    process.stdout.write('\n')
+    process.stdout.write('\n') // print line break
+    // print out welcome message
     process.stdout.write(gradient.passion('Welcome to the Forget Me Not! CLI '))
-    process.stdout.write('\n\n')
+    process.stdout.write('\n\n') // print 2 line breaks
+    // after hook, default messaging will appear
   }
 }
 
