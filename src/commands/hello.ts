60a215b68f1b967ee605c8a71a1e64f2927387bc
Tyler Turnipseed <tyler@techturnip.us>
2020-08-13 16:49:13 -0400

    Added in testing, adjusted travis ci config, adjusted tsconfig, adjusted dependencies

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,49 +1,31 @@
-import {Command, flags} from '@oclif/command'
-
-// Gradient Text Library
-const gradient = require('gradient-string')
-
-// ASCII Font Library
-const figlet = require('figlet')
-
-// Get version from package.json
-const pjson = require('../../package.json')
+import { Command, flags } from "@oclif/command";
 
 export default class Hello extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static examples = [
     `$ fmn hello
 hello world from ./src/hello.ts!
 `,
-  ]
+  ];
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: "f" }),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(Hello)
+    const { args, flags } = this.parse(Hello);
 
-    const ver = pjson.version
-
-    this.log(gradient.passion(figlet.textSync(`FMN CLI v${ver}`, {
-      font: 'Big',
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-      width: 80,
-      whitespaceBreak: true,
-    })))
-
-    this.log('Welcome to Forget Me Not! CLI \n')
+    const name = flags.name ?? "world";
+    this.log(`hello ${name} from ./src/commands/hello.ts`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
