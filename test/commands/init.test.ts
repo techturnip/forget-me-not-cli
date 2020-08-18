ad1afeb731dfde270a97a88cfd0d94a507b976af
Tyler Turnipseed <tyler@techturnip.us>
2020-08-18 14:59:44 -0400

    working on testing init command, generated todo command

--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -1,11 +1,23 @@
 import {/* expect, */test} from '@oclif/test'
 
+const mockFs = require('mock-fs')
+
 describe('init', () => {
+  mockFs({
+    'Project/testProject': {
+      'package.json': JSON.stringify({name: 'testProject', version: '1.0.0', author: 'Test Author'}),
+      'fmnrc.json': JSON.stringify({}),
+    },
+  })
+
   test
-  .stdout()
+  .stdout({print: true})
   .command(['init'])
+  .it()
 
   test
   .stdout()
   .command(['init'])
+
+  mockFs.restore()
 })
