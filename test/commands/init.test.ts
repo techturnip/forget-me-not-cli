5ebef5ea9b438094724a15455fabee4596e0e828
Tyler Turnipseed <tyler@techturnip.us>
2020-08-17 23:27:52 -0400

    removed default test cases from command generation

--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -1,17 +1,11 @@
-import {expect, test} from '@oclif/test'
+import {/* expect, */test} from '@oclif/test'
 
 describe('init', () => {
   test
   .stdout()
   .command(['init'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
 
   test
   .stdout()
-  .command(['init', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+  .command(['init'])
 })
